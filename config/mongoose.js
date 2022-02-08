68de2935d158000d1bfab00bc31e707ffd85ed2b
Smanika <67970197+Smanika@users.noreply.github.com>
2022-02-08 09:51:23 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/login');
+mongoose.connect('ur database url');
 
 const db= mongoose.connection
 db.on('error',console.error.bind(console,'error in database')) // errors
